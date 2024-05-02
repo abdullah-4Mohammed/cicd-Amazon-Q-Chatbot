9754ddcd14db3657eec64b375b0e117515e94084
Abdullah <abdullah.4mohamed@gmail.com>
2024-05-02 19:23:55 +0100

    commenting kendra

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -12,13 +12,13 @@ resource "null_resource" "upload_files" {
   depends_on = [aws_s3_bucket.amazon-q-ds-bucket]
 }
 
-// create amazon kendras index
-resource "aws_kendra_index" "Kendra-Index" {
-  name        = "kendra-index"
-  role_arn    = aws_iam_role.amazon-q-ds.arn
-  description = "Amazon Kendra index for Q&A data source"
-  edition     = "DEVELOPER_EDITION"
-  # server_side_encryption_configuration {
-  #   kms_key_id = aws_kms_key.amazon-q-ds.arn
-  # }
-}
+# // create amazon kendras index
+# resource "aws_kendra_index" "Kendra-Index" {
+#   name        = "kendra-index"
+#   role_arn    = aws_iam_role.amazon-q-ds.arn
+#   description = "Amazon Kendra index for Q&A data source"
+#   edition     = "DEVELOPER_EDITION"
+#   # server_side_encryption_configuration {
+#   #   kms_key_id = aws_kms_key.amazon-q-ds.arn
+#   # }
+# }
