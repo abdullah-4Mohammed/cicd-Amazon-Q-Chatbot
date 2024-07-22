1155199e510a23c2302d6944744492eb61a392b5
Abdullah <abdullah.4mohamed@gmail.com>
2024-07-22 14:58:09 +0100

    do

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,6 +1,7 @@
 // Create the S3 bucket 
 resource "aws_s3_bucket" "amazon-q-ds-bucket" {
   bucket = "${var.serviceName}-datasource-bucket"
+  force_destroy = alltrue()
 
 }
 #"amazon-q-ds-bucket" is the local name of the resource within your Terraform configuration. 
