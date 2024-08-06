829a60dc70231a3daf67e3a911e6e8bacf19a746
Abdullah <abdullah.4mohamed@gmail.com>
2024-08-06 21:38:12 +0100

    fixing the tf s3

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,17 +1,22 @@
-resource "aws_s3_bucket" "amazon-q-ds-bucket" {    
-  bucket = var.datasourceBucketName
-  force_destroy = true
-}
 # "amazon-q-ds-bucket" is the local name of the resource within your Terraform configuration. 
 # This name is used to refer to this resource within your Terraform configuration. 
 # It doesn't have to match the name of the S3 bucket in AWS.
 
+data "archive_file" "data" {
+  type        = "zip"
+  source_dir  = "../../src/data"
+  output_path = "/tmp/data.zip"
+}
 
-// Use a local-exec provisioner to upload the files
 resource "null_resource" "upload_files" {
+  triggers = {
+    data_hash = data.archive_file.data.output_base64sha256
+  }
+
   provisioner "local-exec" {
     command = "aws s3 sync ../../src/data s3://${aws_s3_bucket.amazon-q-ds-bucket.bucket}"
   }
+
   depends_on = [aws_s3_bucket.amazon-q-ds-bucket]
 }
 
